f59cebebc80dd9861fcc5a419ae0b85a4b134833
unknown <victornunesrodrigues27@gmail.com>
2024-05-06 22:04:57 -0300

    first commit

---
+++ media_aritmetica.py
@@ -0,0 +1,9 @@
+# PROGRAMA BÁSICO EM PYHTON: MÉDIA ARITMÉTICA #
+
+n1= float(input("informe sua primeira nota: "))
+n2= float(input("Informe sua segunda nota: "))
+
+
+media = (n1+n2) / 2
+
+print('Sua Média é: ', media)
